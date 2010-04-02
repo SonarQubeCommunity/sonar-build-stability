3a8231cc71266fc8c49fbc64127237bbaf11e3ce
Evgeny Mandrikov <mandrikov@gmail.com>
2010-04-02 14:31:20 +0000

    BUILDSTABILITY: Fix directions for metrics.

--- src/main/java/org/sonar/plugins/buildstability/BuildStabilityMetrics.java
+++ src/main/java/org/sonar/plugins/buildstability/BuildStabilityMetrics.java
@@ -26,167 +26,167 @@ import java.util.List;
  * @author Evgeny Mandrikov
  */
 public class BuildStabilityMetrics implements Metrics {
-  public static final String DOMAIN_BUILD = "Continuous integration";
+    public static final String DOMAIN_BUILD = "Continuous integration";
 
-  /**
-   * Number of builds.
-   */
-  public static final Metric BUILDS = new Metric(
-      "builds",
-      "Builds",
-      "Number of builds",
-      Metric.ValueType.INT,
-      Metric.DIRECTION_NONE,
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * Number of failed builds.
-   */
-  public static final Metric FAILED = new Metric(
-      "build_failures",
-      "Failed Builds",
-      "Number of failed builds",
-      Metric.ValueType.INT,
-      Metric.DIRECTION_WORST,
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * Ratio of successful builds. Measured as percentage of successful builds out of all last builds.
-   */
-  public static final Metric SUCCESS_RATE = new Metric(
-      "build_success_density",
-      "Success Rate (%)",
-      "Ratio of successful builds",
-      Metric.ValueType.PERCENT,
-      Metric.DIRECTION_BETTER,
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * Build average duration. Includes only duration of successful builds.
-   */
-  public static final Metric AVG_DURATION = new Metric(
-      "build_average_duration",
-      "Average Duration",
-      "Average Duration",
-      Metric.ValueType.MILLISEC,
-      Metric.DIRECTION_WORST,
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * Duration of longest successful build.
-   */
-  public static final Metric LONGEST_DURATION = new Metric(
-      "build_longest_duration",
-      "Longest duration",
-      "Duration of longest successful build",
-      Metric.ValueType.MILLISEC,
-      Metric.DIRECTION_NONE, // TODO
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * Duration of shortest successful build.
-   */
-  public static final Metric SHORTEST_DURATION = new Metric(
-      "build_shortest_duration",
-      "Shortest duration",
-      "Duration of shortest successful build",
-      Metric.ValueType.MILLISEC,
-      Metric.DIRECTION_NONE, // TODO
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * Average time to fix a failure.
-   */
-  public static final Metric AVG_TIME_TO_FIX = new Metric(
-      "build_average_time_to_fix_failure",
-      "Average time to fix a failure",
-      "Average time to fix a failure",
-      Metric.ValueType.MILLISEC,
-      Metric.DIRECTION_NONE, // TODO
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * Longest time to fix a failure.
-   */
-  public static final Metric LONGEST_TIME_TO_FIX = new Metric(
-      "build_longest_time_to_fix_failure",
-      "Longest time to fix a failure",
-      "Longest time to fix a failure",
-      Metric.ValueType.MILLISEC,
-      Metric.DIRECTION_NONE, // TODO
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * Average number of builds between fixes.
-   */
-  public static final Metric AVG_BUILDS_TO_FIX = new Metric(
-      "build_average_builds_to_fix_failure",
-      "Average number of builds between fixes",
-      "Average number of builds between fixes",
-      Metric.ValueType.INT,
-      Metric.DIRECTION_NONE, // TODO
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * TODO comment me (seconds)
-   */
-  public static final Metric DURATIONS = new Metric(
-      "build_durations",
-      "Durations",
-      "Durations",
-      Metric.ValueType.DATA,
-      Metric.DIRECTION_NONE,
-      false,
-      DOMAIN_BUILD
-  );
-
-  /**
-   * TODO comment me
-   */
-  public static final Metric RESULTS = new Metric(
-      "build_results",
-      "Results",
-      "Results",
-      Metric.ValueType.DATA,
-      Metric.DIRECTION_NONE,
-      false,
-      DOMAIN_BUILD
-  );
-
-  public List<Metric> getMetrics() {
-    return Arrays.asList(
-        BUILDS,
-        FAILED,
-        SUCCESS_RATE,
-
-        AVG_DURATION,
-        LONGEST_DURATION,
-        SHORTEST_DURATION,
-
-        AVG_TIME_TO_FIX,
-        LONGEST_TIME_TO_FIX,
-        AVG_BUILDS_TO_FIX,
-
-        DURATIONS,
-        RESULTS
+    /**
+     * Number of builds.
+     */
+    public static final Metric BUILDS = new Metric(
+            "builds",
+            "Builds",
+            "Number of builds",
+            Metric.ValueType.INT,
+            Metric.DIRECTION_NONE,
+            false,
+            DOMAIN_BUILD
     );
-  }
+
+    /**
+     * Number of failed builds.
+     */
+    public static final Metric FAILED = new Metric(
+            "build_failures",
+            "Failed Builds",
+            "Number of failed builds",
+            Metric.ValueType.INT,
+            Metric.DIRECTION_WORST,
+            false,
+            DOMAIN_BUILD
+    );
+
+    /**
+     * Ratio of successful builds. Measured as percentage of successful builds out of all last builds.
+     */
+    public static final Metric SUCCESS_RATE = new Metric(
+            "build_success_density",
+            "Success Rate (%)",
+            "Ratio of successful builds",
+            Metric.ValueType.PERCENT,
+            Metric.DIRECTION_BETTER,
+            false,
+            DOMAIN_BUILD
+    );
+
+    /**
+     * Build average duration. Includes only duration of successful builds.
+     */
+    public static final Metric AVG_DURATION = new Metric(
+            "build_average_duration",
+            "Average Duration",
+            "Average Duration",
+            Metric.ValueType.MILLISEC,
+            Metric.DIRECTION_WORST,
+            false,
+            DOMAIN_BUILD
+    );
+
+    /**
+     * Duration of longest successful build.
+     */
+    public static final Metric LONGEST_DURATION = new Metric(
+            "build_longest_duration",
+            "Longest duration",
+            "Duration of longest successful build",
+            Metric.ValueType.MILLISEC,
+            Metric.DIRECTION_WORST,
+            false,
+            DOMAIN_BUILD
+    );
+
+    /**
+     * Duration of shortest successful build.
+     */
+    public static final Metric SHORTEST_DURATION = new Metric(
+            "build_shortest_duration",
+            "Shortest duration",
+            "Duration of shortest successful build",
+            Metric.ValueType.MILLISEC,
+            Metric.DIRECTION_WORST,
+            false,
+            DOMAIN_BUILD
+    );
+
+    /**
+     * Average time to fix a failure.
+     */
+    public static final Metric AVG_TIME_TO_FIX = new Metric(
+            "build_average_time_to_fix_failure",
+            "Average time to fix a failure",
+            "Average time to fix a failure",
+            Metric.ValueType.MILLISEC,
+            Metric.DIRECTION_WORST,
+            false,
+            DOMAIN_BUILD
+    );
+
+    /**
+     * Longest time to fix a failure.
+     */
+    public static final Metric LONGEST_TIME_TO_FIX = new Metric(
+            "build_longest_time_to_fix_failure",
+            "Longest time to fix a failure",
+            "Longest time to fix a failure",
+            Metric.ValueType.MILLISEC,
+            Metric.DIRECTION_WORST,
+            false,
+            DOMAIN_BUILD
+    );
+
+    /**
+     * Average number of builds between fixes.
+     */
+    public static final Metric AVG_BUILDS_TO_FIX = new Metric(
+            "build_average_builds_to_fix_failure",
+            "Average number of builds between fixes",
+            "Average number of builds between fixes",
+            Metric.ValueType.INT,
+            Metric.DIRECTION_WORST,
+            false,
+            DOMAIN_BUILD
+    );
+
+    /**
+     * TODO comment me (seconds)
+     */
+    public static final Metric DURATIONS = new Metric(
+            "build_durations",
+            "Durations",
+            "Durations",
+            Metric.ValueType.DATA,
+            Metric.DIRECTION_NONE,
+            false,
+            DOMAIN_BUILD
+    );
+
+    /**
+     * TODO comment me
+     */
+    public static final Metric RESULTS = new Metric(
+            "build_results",
+            "Results",
+            "Results",
+            Metric.ValueType.DATA,
+            Metric.DIRECTION_NONE,
+            false,
+            DOMAIN_BUILD
+    );
+
+    public List<Metric> getMetrics() {
+        return Arrays.asList(
+                BUILDS,
+                FAILED,
+                SUCCESS_RATE,
+
+                AVG_DURATION,
+                LONGEST_DURATION,
+                SHORTEST_DURATION,
+
+                AVG_TIME_TO_FIX,
+                LONGEST_TIME_TO_FIX,
+                AVG_BUILDS_TO_FIX,
+
+                DURATIONS,
+                RESULTS
+        );
+    }
 }
